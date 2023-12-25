b3797e0f9aa25bce8dd3b5ac2ff3684e76f740cc
faustojc <faustojcboko@gmail.com>
2023-12-26 02:24:27 +0800

    Added other fields and fixed layout

--- lib/widgets/auth/register.dart
+++ lib/widgets/auth/register.dart
@@ -11,24 +11,32 @@ class RegisterForm extends StatefulWidget {
 
 class _RegisterFormState extends State<RegisterForm> {
   final List<Widget> _forms = <Widget>[
-    const EmailPassForm(key: ValueKey('email_pass_form')),
-    const AthleteForm(key: ValueKey('athlete_form')),
+    const EmailPassForm(key: ValueKey(1)),
+    const AthleteForm(key: ValueKey(2)),
   ];
 
   int _currentIndex = 0;
   late Widget _currentForm = _forms[_currentIndex];
 
   void _setForm() {
-    setState(() {
-      _currentIndex = (_currentIndex + 1) % _forms.length;
-      _currentForm = _forms[_currentIndex];
-    });
+    if (_currentIndex == 0) {
+      setState(() {
+        _currentIndex = (_currentIndex + 1) % _forms.length;
+        _currentForm = KeyedSubtree(
+          key: ValueKey(_currentIndex),
+          child: _forms[_currentIndex],
+        );
+      });
+    }
   }
 
   void _previousForm() {
     setState(() {
       _currentIndex = (_currentIndex - 1) % _forms.length;
-      _currentForm = _forms[_currentIndex];
+      _currentForm = KeyedSubtree(
+        key: ValueKey(_currentIndex),
+        child: _forms[_currentIndex],
+      );
     });
   }
 
@@ -41,27 +49,42 @@ class _RegisterFormState extends State<RegisterForm> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              child: IconButton(
-                onPressed: _previousForm,
-                icon: const Icon(Icons.arrow_back),
+            Text('Register', style: Theme.of(context).textTheme.headlineMedium),
+            const SizedBox(height: 20),
+            Flexible(
+              flex: 0,
+              fit: FlexFit.loose,
+              child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 150),
+                  transitionBuilder: (child, animation) => ScaleTransition(scale: animation, child: child),
+                  switchInCurve: Curves.easeIn,
+                  switchOutCurve: Curves.easeOut,
+                  child: _currentForm),
+            ),
+            const SizedBox(height: 30),
+            Flexible(
+              fit: FlexFit.loose,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _currentIndex == 1 ? Expanded(child: ElevatedButton(onPressed: _previousForm, child: const Text('Back'))) : const SizedBox(),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: _setForm,
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: _currentIndex == 0 ? Colors.deepPurple : Colors.white,
+                        backgroundColor: _currentIndex == 0 ? Colors.white : Colors.deepPurple,
+                      ),
+                      child: Text(_currentIndex == 0 ? 'Next' : 'Register'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            AnimatedSwitcher(
-                duration: const Duration(milliseconds: 150), transitionBuilder: (child, animation) => ScaleTransition(scale: animation, child: child), switchInCurve: Curves.easeIn, switchOutCurve: Curves.easeOut, child: _currentForm),
-            const SizedBox(height: 10),
-            ElevatedButton(
-                onPressed: _setForm,
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.deepPurple,
-                  maximumSize: const Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-                child: Text(_currentIndex == 0 ? 'Next' : 'Register')),
             const SizedBox(height: 30),
             TextButton(
-              onPressed: () => Routes.to(context, '/login'),
+              onPressed: () => Routes.redirectTo(context, "/login"),
               child: const Text('Already have an account? Login'),
             )
           ],
@@ -106,32 +129,32 @@ class _EmailPassFormState extends State<EmailPassForm> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      elevation: 5,
-      surfaceTintColor: Colors.deepPurple,
-      borderRadius: BorderRadius.circular(10),
       child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                InputFormField(
-                  type: InputFormFieldType.text,
-                  label: 'Email',
-                  controller: _emailController,
-                  validator: _validateEmail,
-                ),
-                const SizedBox(height: 20),
-                InputFormField(
-                  type: InputFormFieldType.password,
-                  label: 'Password',
-                  controller: _passwordController,
-                  validator: _validatePassword,
-                ),
-              ],
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            InputFormField(
+              type: FieldType.text,
+              label: 'Email',
+              controller: _emailController,
+              validator: _validateEmail,
+              icon: Icons.email_outlined,
+              decoration: FieldDecoration.borderless,
             ),
-          )),
+            const SizedBox(height: 20),
+            InputFormField(
+              type: FieldType.password,
+              label: 'Password',
+              controller: _passwordController,
+              validator: _validatePassword,
+              icon: Icons.lock_outline,
+              decoration: FieldDecoration.borderless,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -147,34 +170,95 @@ class _AthleteFormState extends State<AthleteForm> {
   final _formKey = GlobalKey<FormState>();
   final _firstNameController = TextEditingController();
   final _lastNameController = TextEditingController();
+  final _genderController = TextEditingController();
+  final _ageController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  final List<String> genderList = ['Male', 'Female', 'Others'];
+
+  late String _birthday = '';
+
+  void _setBirthday(DateTime date) {
+    setState(() {
+      final formattedDate = date.toString().split(' ')[0].split('-');
+      _birthday = '${formattedDate[1]}/${formattedDate[2]}/${formattedDate[0]}';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      elevation: 5,
-      borderRadius: BorderRadius.circular(10),
-      surfaceTintColor: Colors.deepPurple,
       child: Form(
         key: _formKey,
-        child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InputFormField(
+              type: FieldType.text,
+              label: 'First Name',
+              controller: _firstNameController,
+              decoration: FieldDecoration.borderless,
+            ),
+            const SizedBox(height: 20),
+            InputFormField(
+              type: FieldType.text,
+              label: 'Last Name',
+              controller: _lastNameController,
+              decoration: FieldDecoration.borderless,
+            ),
+            const SizedBox(height: 20),
+            InputFormField(
+              type: FieldType.int,
+              label: 'Age',
+              controller: _ageController,
+              decoration: FieldDecoration.borderless,
+            ),
+            const SizedBox(height: 20),
+            InputFormField(
+              type: FieldType.double,
+              label: 'Weight',
+              hint: 'Enter your weight in lbs',
+              controller: _weightController,
+              decoration: FieldDecoration.borderless,
+            ),
+            const SizedBox(height: 20),
+            DropdownButtonFormField(
+              hint: const Text('Select Gender'),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+              ),
+              items: genderList.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? value) => setState(() => _genderController.text = value!),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                InputFormField(
-                  type: InputFormFieldType.text,
-                  label: 'First Name',
-                  controller: _firstNameController,
-                ),
-                InputFormField(
-                  type: InputFormFieldType.text,
-                  label: 'Last Name',
-                  controller: _lastNameController,
+                const Text("Birthday: ", style: TextStyle(fontSize: 16)),
+                const SizedBox(width: 10),
+                FilledButton.tonal(
+                  onPressed: () => showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime.now(),
+                  )
+                      .then((DateTime? date) => _setBirthday(date!))
+                      .catchError((err) => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Something went wrong! Try again later.")))),
+                  child: Text(_birthday == '' ? 'Select your birthday' : _birthday.toString(), style: const TextStyle(color: Colors.deepPurple)),
                 ),
               ],
-            )),
+            ),
+          ],
+        ),
       ),
     );
   }
