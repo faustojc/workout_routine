318961621be2a28bc19056de2805dac791cd8c8d
faustojc <faustojcboko@gmail.com>
2023-12-26 02:25:01 +0800

    Added data location.dart

---
+++ lib/data/location.dart
@@ -0,0 +1,39 @@
+final Map<String, String> regions = {
+  "Region I": "Ilocos Region",
+  "Region II": "Cagayan Valley",
+  "Region III": "Central Luzon",
+  "Region IV-A": "CALABARZON",
+  "Region IV-B": "MIMAROPA",
+  "Region V": "Bicol Region",
+  "Region VI": "Western Visayas",
+  "Region VII": "Central Visayas",
+  "Region VIII": "Eastern Visayas",
+  "Region IX": "Zamboanga Peninsula",
+  "Region X": "Northern Mindanao",
+  "Region XI": "Davao Region",
+  "Region XII": "SOCCSKSARGEN",
+  "Region XIII": "Caraga",
+  "BARMM": "Bangsamoro Autonomous Region in Muslim Mindanao",
+  "CAR": "Cordillera Administrative Region",
+  "NCR": "National Capital Region"
+};
+
+final Map<String, List<String>> cities = {
+  "Region I": ["Ilocos Norte", "Ilocos Sur", "La Union", "Pangasinan"],
+  "Region II": ["Batanes", "Cagayan", "Isabela", "Nueva Vizcaya", "Quirino", "Santiago"],
+  "Region III": ["Aurora", "Bataan", "Bulacan", "Nueva Ecija", "Pampanga", "Tarlac", "Zambales"],
+  "Region IV-A": ["Batangas", "Cavite", "Laguna", "Quezon", "Rizal"],
+  "Region IV-B": ["Marinduque", "Occidental Mindoro", "Oriental Mindoro", "Palawan", "Puerto Princesa", "Romblon"],
+  "Region V": ["Albay", "Camarines Norte", "Camarines Sur", "Catanduanes", "Masbate", "Naga", "Sorsogon"],
+  "Region VI": [],
+  "Region VII": [],
+  "Region VIII": [],
+  "Region IX": [],
+  "Region X": [],
+  "Region XI": [],
+  "Region XII": [],
+  "Region XIII": [],
+  "BARMM": [],
+  "CAR": [],
+  "NCR": []
+};
