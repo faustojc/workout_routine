dfde8ae69d7e7eede21261b04c2a6c5ff436646e
faustojc <faustojcboko@gmail.com>
2024-01-09 01:57:11 +0800

    Firestore update and subscription

--- lib/models/athletes.dart
+++ lib/models/athletes.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Athlete {
   late String? userId;
   final String email;
@@ -10,6 +12,7 @@ class Athlete {
   final String age;
   final String weight;
   final String height;
+  final bool isSubscribed;
   final DateTime birthday;
   final DateTime createdAt;
   final DateTime updatedAt;
@@ -29,9 +32,54 @@ class Athlete {
     required this.birthday,
     required this.createdAt,
     required this.updatedAt,
+    this.isSubscribed = false,
   });
 
   static Athlete? current;
+
+  factory Athlete.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options) {
+    final data = snapshot.data();
+
+    return Athlete(
+      userId: data?['userId'],
+      email: data?['email'],
+      password: data?['password'],
+      firstName: data?['firstName'],
+      lastName: data?['lastName'],
+      gender: data?['gender'],
+      city: data?['city'],
+      address: data?['address'],
+      age: data?['age'],
+      weight: data?['weight'],
+      height: data?['height'],
+      isSubscribed: data?['isSubscribed'],
+      birthday: data?['birthday'],
+      createdAt: data?['createdAt'],
+      updatedAt: data?['updatedAt'],
+    );
+  }
+
+  factory Athlete.fromFireStore(QuerySnapshot<Map<String, dynamic>> snapshot) {
+    final data = snapshot.docs.first.data();
+
+    return Athlete(
+      userId: data['userId'],
+      email: data['email'],
+      password: data['password'],
+      firstName: data['firstName'],
+      lastName: data['lastName'],
+      gender: data['gender'],
+      city: data['city'],
+      address: data['address'],
+      age: data['age'],
+      weight: data['weight'],
+      height: data['height'],
+      isSubscribed: data['isSubscribed'],
+      birthday: data['birthday'],
+      createdAt: data['createdAt'],
+      updatedAt: data['updatedAt'],
+    );
+  }
 
   factory Athlete.fromJson(Map<String, dynamic> json) {
     return Athlete(
@@ -46,6 +94,7 @@ class Athlete {
       age: json['age'],
       weight: json['weight'],
       height: json['height'],
+      isSubscribed: json['isSubscribed'],
       birthday: json['birthday'],
       createdAt: json['createdAt'],
       updatedAt: json['updatedAt'],
@@ -64,6 +113,7 @@ class Athlete {
         'age': age,
         'weight': weight,
         'height': height,
+        'isSubscribed': isSubscribed,
         'birthdate': birthday,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
