3bebf46c453164baf79dbcfe41779dcf41abe2ab
faustojc <faustojcboko@gmail.com>
2024-01-11 21:58:26 +0800

    Fixed back button route issue

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:workout_routine/routes.dart';
 import 'package:workout_routine/themes/colors.dart';
+import 'package:workout_routine/widgets/auth/main_auth.dart';
+import 'package:workout_routine/widgets/components/loading.dart';
+import 'package:workout_routine/widgets/dashboard/user/dashboard.dart';
 
 import 'firebase_options.dart';
 
@@ -26,7 +29,20 @@ class MainApp extends StatelessWidget {
         fontFamily: 'SpaceGrotesk',
         useMaterial3: true,
       ),
-      initialRoute: FirebaseAuth.instance.currentUser != null ? '/' : '/auth',
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Loading();
+          } else {
+            if (snapshot.hasData) {
+              return const Dashboard();
+            } else {
+              return const MainAuth();
+            }
+          }
+        },
+      ),
       onGenerateRoute: Routes.generateRoute,
       debugShowCheckedModeBanner: false,
     );
