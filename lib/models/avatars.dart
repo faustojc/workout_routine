4e8b2e1c905465d3e93cb1c9e971990b69243028
faustojc <faustojcboko@gmail.com>
2024-01-11 19:36:58 +0800

    Change field to userId

--- lib/models/avatars.dart
+++ lib/models/avatars.dart
@@ -1,28 +1,28 @@
 class Avatar {
-  final String id;
   final String url;
+  final String userId;
   final String createdAt;
   final String updatedAt;
 
   Avatar({
-    required this.id,
     required this.url,
+    required this.userId,
     required this.createdAt,
     required this.updatedAt,
   });
 
   factory Avatar.fromJson(Map<String, dynamic> json) {
     return Avatar(
-      id: json['id'],
       url: json['url'],
+      userId: json['userId'],
       createdAt: json['createdAt'],
       updatedAt: json['updatedAt'],
     );
   }
 
   Map<String, dynamic> toJson() => {
-        'id': id,
         'url': url,
+        'athleteId': userId,
         'createdAt': createdAt,
         'updatedAt': updatedAt,
       };
--- lib/models/subscription.dart
+++ lib/models/subscription.dart
@@ -1,33 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Subscription {
-  final String athleteId;
+  final String userId;
   final String description;
   final String price;
   final String duration;
   final String type;
+  final Timestamp dateSubscribed;
 
   Subscription({
-    required this.athleteId,
+    required this.userId,
     required this.description,
     required this.price,
     required this.duration,
     required this.type,
+    required this.dateSubscribed,
   });
 
   factory Subscription.fromJson(Map<String, dynamic> json) {
     return Subscription(
-      athleteId: json['athleteId'],
+      userId: json['userId'],
       description: json['description'],
       price: json['price'],
       duration: json['duration'],
       type: json['type'],
+      dateSubscribed: json['dateSubscribed'],
     );
   }
 
   Map<String, dynamic> toJson() => {
-        'athleteId': athleteId,
+        'userId': userId,
         'description': description,
         'price': price,
         'duration': duration,
         'type': type,
+        'dateSubscribed': dateSubscribed,
       };
 }
