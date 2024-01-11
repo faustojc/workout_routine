8d588355d46c59e9c1649533648e6d37c04cb2c3
faustojc <faustojcboko@gmail.com>
2024-01-11 17:51:44 +0800

    Added Loading widget layout

--- lib/widgets/components/loading.dart
+++ lib/widgets/components/loading.dart
@@ -1,1 +1,59 @@
-class Loading {}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:workout_routine/themes/colors.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget splashScreen(BuildContext context) => Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [ThemeColor.primary, ThemeColor.tertiary],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: ScaleTransition(
+            scale: _animation,
+            child: Image.asset(
+              'assets/images/icons/logo-white.png',
+              width: 200,
+              height: 200,
+            ),
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return splashScreen(context);
+  }
+}
