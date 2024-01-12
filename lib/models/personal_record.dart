5df030e868d0c5c1b6e242d172dc0f8a04d63854
faustojc <faustojcboko@gmail.com>
2024-01-12 21:53:21 +0800

    Added fields

--- lib/models/personal_record.dart
+++ lib/models/personal_record.dart
@@ -1,1 +1,38 @@
-class PersonalRecord {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class PersonalRecord {
+  final String userId;
+  final String title;
+  final double weight;
+  final double oldWeight;
+  final Timestamp createdAt;
+  final Timestamp updatedAt;
+
+  PersonalRecord({
+    required this.userId,
+    required this.title,
+    required this.weight,
+    required this.oldWeight,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  static List<PersonalRecord?> current = [];
+
+  factory PersonalRecord.fromFirestoreDocument(DocumentSnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options) {
+    final data = snapshot.data();
+
+    return PersonalRecord(
+      userId: data?['userId'],
+      title: data?['title'],
+      weight: data?['weight'],
+      oldWeight: data?['oldWeight'],
+      createdAt: data?['createdAt'],
+      updatedAt: data?['updatedAt'],
+    );
+  }
+
+  static List<PersonalRecord> fromFirestoreQuery(QuerySnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options) {
+    return snapshot.docs.map((doc) => PersonalRecord.fromFirestoreDocument(doc, options)).toList();
+  }
+}
