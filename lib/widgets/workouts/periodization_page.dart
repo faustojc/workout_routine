f6cc3b78f9ad904fe45cc7c1beadcf9dfc45f994
faustojc <faustojcboko@gmail.com>
2024-01-20 16:02:54 +0800

    Fixed the layout errors

--- lib/widgets/workouts/periodization_page.dart
+++ lib/widgets/workouts/periodization_page.dart
@@ -6,24 +6,18 @@ class PeriodizationPage extends StatelessWidget {
   const PeriodizationPage({super.key});
 
   Widget _emptyPeriodization() {
-    return Container(
-      height: 200,
-      width: 200,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.transparent,
-        border: Border.all(color: ThemeColor.accent, width: 2),
-      ),
+    return Align(
+      alignment: Alignment.center,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset('assets/images/icons/empty-content.png', scale: 0.8),
-          const SizedBox(height: 10),
           const Text(
             'No workouts yet',
             style: TextStyle(
-              fontSize: 14,
+              fontSize: 16,
               fontWeight: FontWeight.w700,
+              color: ThemeColor.white,
             ),
           ),
           const SizedBox(height: 6),
@@ -33,6 +27,7 @@ class PeriodizationPage extends StatelessWidget {
             style: TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w300,
+              color: ThemeColor.white,
             ),
           ),
         ],
@@ -42,50 +37,73 @@ class PeriodizationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (PeriodizationModel.list.isEmpty) {
-      return _emptyPeriodization();
-    }
-
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'Start Workout',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w700,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Periodization'),
+        centerTitle: true,
+        backgroundColor: ThemeColor.primary,
+        foregroundColor: ThemeColor.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(20.0),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              ThemeColor.primary,
+              ThemeColor.secondary,
+            ],
           ),
-          Align(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: PeriodizationModel.list
-                  .map(
-                    (periodization) => ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: ThemeColor.secondary,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                        ),
-                        child: Text(
-                          periodization.acronym ?? periodization.name,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: ThemeColor.white,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        )),
-                  )
-                  .toList(),
-            ),
-          ),
-        ],
+        ),
+        child: (PeriodizationModel.list.isEmpty)
+            ? _emptyPeriodization()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    'Start Workout',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: PeriodizationModel.list
+                          .map(
+                            (periodization) => ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: ThemeColor.secondary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                ),
+                                child: Text(
+                                  periodization.acronym ?? periodization.name,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: ThemeColor.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                )),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
