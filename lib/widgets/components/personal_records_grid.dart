2e74a225ea9eef64f39c11aa05a811c1fd463b27
faustojc <faustojcboko@gmail.com>
2024-01-20 16:03:26 +0800

    Added conditional widget for empty data

--- lib/widgets/components/personal_records_grid.dart
+++ lib/widgets/components/personal_records_grid.dart
@@ -43,6 +43,40 @@ class _PersonalRecordsGridState extends State<PersonalRecordsGrid> {
 
   @override
   Widget build(BuildContext context) {
+    if (PersonalRecordModel.list.isEmpty) {
+      return Container(
+          padding: const EdgeInsets.all(20.0),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            border: Border.all(color: ThemeColor.tertiary),
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.transparent,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/icons/empty-content.png', height: 100),
+              const Text(
+                'Empty personal records',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: ThemeColor.white,
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Start adding personal records to tract it here',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300,
+                  color: ThemeColor.white,
+                ),
+              ),
+            ],
+          ));
+    }
+
     return GridView.count(
       crossAxisCount: _personalRecordsCount,
       physics: const NeverScrollableScrollPhysics(),
@@ -51,13 +85,14 @@ class _PersonalRecordsGridState extends State<PersonalRecordsGrid> {
       crossAxisSpacing: 2,
       children: PersonalRecordModel.list.where((record) => record.userId == UserModel.current!.id).map((record) {
         return Container(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
           constraints: (_personalRecordsCount == 1) ? const BoxConstraints(maxHeight: 350, maxWidth: 350) : const BoxConstraints(maxHeight: 200, maxWidth: 200),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             color: ThemeColor.accent,
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
@@ -66,37 +101,48 @@ class _PersonalRecordsGridState extends State<PersonalRecordsGrid> {
                   Text(
                     record.title,
                     style: const TextStyle(
-                      fontSize: 12,
+                      fontSize: 14,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                  IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_forward_ios_rounded)),
                 ],
               ),
-              const SizedBox(height: 10),
-              ListTile(
-                leading: Text(
-                  _getRecentPRHistory(record.id).weight.toString(),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+              Row(
+                children: [
+                  Text(
+                    _getRecentPRHistory(record.id).weight.toString(),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                title: Text(_dateFormatter(_getRecentPRHistory(record.id).createdAt)),
-                subtitle: const Text(
-                  'lbs',
-                  style: TextStyle(fontSize: 12),
-                ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _dateFormatter(_getRecentPRHistory(record.id).createdAt),
+                        style: const TextStyle(fontSize: 10),
+                      ),
+                      const Text(
+                        'lbs',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              const SizedBox(height: 10),
               ActionChip(
                 onPressed: () {},
+                backgroundColor: ThemeColor.accent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 4),
                 label: const Text(
                   'View History',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w600,
-                  ),
+                  textScaler: TextScaler.linear(0.7),
                 ),
               ),
             ],
