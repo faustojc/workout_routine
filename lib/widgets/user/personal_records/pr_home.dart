decb11d560731d9f6223f62eebc80c1acb7c1b4b
faustojc <faustojcboko@gmail.com>
2024-01-23 21:36:41 +0800

    Added layout and content

--- lib/widgets/user/personal_records/pr_home.dart
+++ lib/widgets/user/personal_records/pr_home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:workout_routine/models/personal_record.dart';
+import 'package:workout_routine/models/personal_record_history.dart';
 import 'package:workout_routine/themes/colors.dart';
+import 'package:workout_routine/widgets/components/empty_content.dart';
 
 class PRHome extends StatefulWidget {
   const PRHome({super.key});
@@ -9,11 +13,24 @@ class PRHome extends StatefulWidget {
 }
 
 class _PRHomeState extends State<PRHome> {
+  final List<PersonalRecordModel> _pr = PersonalRecordModel.list;
+  final List<PRHistoryModel> _prHistory = PRHistoryModel.list;
+
+  String _dateFormat(DateTime date) {
+    return DateFormat.yMMMMd('en_US').format(date);
+  }
+
+  String _historiesCount(String prId) {
+    final histories = _prHistory.where((history) => (history.prId == prId)).toList();
+
+    return '${histories.length}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Personal Record'),
+        title: const Text('Personal Records'),
         centerTitle: true,
         backgroundColor: ThemeColor.primary,
         foregroundColor: ThemeColor.white,
@@ -25,10 +42,94 @@ class _PRHomeState extends State<PRHome> {
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.menu),
+            icon: const Icon(Icons.edit),
           ),
         ],
       ),
+      body: Container(
+          padding: const EdgeInsets.all(20.0),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [ThemeColor.primary, ThemeColor.secondary],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Column(
+            children: [
+              SearchBar(
+                hintText: 'Search personal records',
+                leading: const Icon(Icons.search),
+                constraints: const BoxConstraints(
+                  minHeight: 50,
+                  maxHeight: 50,
+                ),
+                textStyle: MaterialStateProperty.all(const TextStyle(
+                  fontSize: 12,
+                  color: ThemeColor.black,
+                )),
+                padding: MaterialStateProperty.all(const EdgeInsets.symmetric(
+                  horizontal: 10,
+                )),
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 20),
+              (_pr.isEmpty)
+                  ? const EmptyContent(title: 'No personal records', subtitle: 'Add your personal records or no search results found')
+                  : Expanded(
+                      child: ListView.builder(
+                          itemCount: _pr.length,
+                          itemBuilder: (context, int index) {
+                            return Card(
+                                elevation: 2,
+                                color: ThemeColor.accent,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 12,
+                                    horizontal: 16,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            _pr[index].title,
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w700,
+                                              color: ThemeColor.black,
+                                            ),
+                                          ),
+                                          Text(
+                                            'Created ${_dateFormat(_pr[index].createdAt)}',
+                                            style: const TextStyle(
+                                              fontSize: 11,
+                                              color: ThemeColor.black,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 10),
+                                          Text(
+                                            'Histories: ${_historiesCount(_pr[index].id)}',
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: ThemeColor.black.withOpacity(0.9),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.arrow_forward_ios_rounded),
+                                      ),
+                                    ],
+                                  ),
+                                ));
+                          }),
+                    )
+            ],
+          )),
     );
   }
 }
