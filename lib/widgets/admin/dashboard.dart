54cb32cf690f22f8deddfdf89f655547f90e1aed
faustojc <faustojcboko@gmail.com>
2024-01-28 22:16:17 +0800

    Created dashboard layout

---
+++ lib/widgets/admin/dashboard.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late Future<Map<String, dynamic>?> _fetchData;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => FutureBuilder(
+      future: _fetchData,
+      builder: (context, snapshot) {
+        return Container();
+      });
+}
