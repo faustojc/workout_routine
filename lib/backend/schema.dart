5f64a9d801c8349dd16ec5b9e2e3baef731c8bc9
faustojc <faustojcboko@gmail.com>
2024-01-28 22:13:05 +0800

    Created schema for PowerSync sqlite database

---
+++ lib/backend/schema.dart
@@ -0,0 +1,88 @@
+import 'package:powersync/powersync.dart';
+
+const schema = Schema([
+  Table('periodizations', [
+    Column.text('name'),
+    Column.text('acronym'),
+    Column.text('description'),
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+  ]),
+  Table('weeks', [
+    Column.text('periodizationId'),
+    Column.text('title'),
+    Column.text('subtitle'),
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+  ]),
+  Table('days', [
+    Column.text('weeksId'),
+    Column.text('title'),
+    Column.text('subtitle'),
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+  ]),
+  Table('workouts', [
+    Column.text('title'),
+    Column.text('description'),
+    Column.text('videoUrl'),
+    Column.text('thumbnailUrl'),
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+    Column.text('daysId')
+  ]),
+  Table('workout_parameters', [
+    Column.text('workoutId'),
+    Column.text('name'),
+    Column.text('value'),
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+  ]),
+  Table('athletes', [
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+    Column.text('firstName'),
+    Column.text('lastName'),
+    Column.text('gender'),
+    Column.real('weight'),
+    Column.real('height'),
+    Column.text('city'),
+    Column.text('address'),
+    Column.text('birthday'),
+    Column.text('userId'),
+    Column.integer('age')
+  ]),
+  Table('category', [
+    Column.text('created_at'),
+    Column.text('updated_at'),
+    Column.text('name'),
+    Column.text('userId'),
+  ]),
+  Table('personal_records', [
+    Column.text('title'),
+    Column.text('createdAt'),
+    Column.text('updatedAt'),
+    Column.text('userId'),
+  ]),
+  Table('personal_records_history', [
+    Column.text('userId'),
+    Column.text('prId'),
+    Column.real('weight'),
+    Column.text('createdAt'),
+  ]),
+  Table('user_workouts', [
+    Column.text('userId'),
+    Column.text('workoutId'),
+    Column.text('playedAt'),
+    Column.text('updatedAt'),
+    Column.text('createdAt'),
+  ]),
+  Table('subscriptions', [
+    Column.real('price'),
+    Column.text('duration'),
+    Column.text('dateSubscribed'),
+    Column.text('dateExpired'),
+    Column.text('userId'),
+    Column.integer('isSubscribed'),
+  ])
+]);
