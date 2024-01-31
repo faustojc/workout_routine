d1d8ed08482ccd459956828c1815e806f8c7ea61
faustojc <faustojcboko@gmail.com>
2024-01-31 22:16:50 +0800

    Improve ui/ux

--- lib/widgets/user/home.dart
+++ lib/widgets/user/home.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:workout_routine/models/athletes.dart';
 import 'package:workout_routine/routes.dart';
+import 'package:workout_routine/services/connectivity_service.dart';
 import 'package:workout_routine/themes/colors.dart';
 import 'package:workout_routine/widgets/components/personal_records_grid.dart';
 import 'package:workout_routine/widgets/components/recent_workout.dart';
+import 'package:workout_routine/widgets/components/toast.dart';
+import 'package:workout_routine/widgets/user/personal_records/pr_home.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,9 +16,23 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  final PageController _pageController = PageController();
+  late ConnectivityService _connectionStatus;
   int _currentIndex = 0;
   double _indicatorLeft = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _connectionStatus = Provider.of<ConnectivityService>(context, listen: false);
+    _connectionStatus.addListener(() {
+      if (_connectionStatus.status == InternetStatus.offline) {
+        showToast(context: context, message: "No connection", type: ToastType.info, vsync: this);
+      }
+    });
+  }
 
   void _setIndex(int value, double itemWidth) {
     setState(() {
@@ -51,20 +69,32 @@ class _HomeState extends State<Home> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   IconButton(
-                    onPressed: () => _setIndex(0, itemWidth),
+                    onPressed: () {
+                      _setIndex(0, itemWidth);
+                      _pageController.animateToPage(0, duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+                    },
                     icon: Icon(Icons.home, color: _currentIndex == 0 ? ThemeColor.accent : ThemeColor.black),
                   ),
                   IconButton(
-                    onPressed: () => _setIndex(1, itemWidth),
+                    onPressed: () {
+                      _setIndex(1, itemWidth);
+                      _pageController.animateToPage(1, duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+                    },
                     icon: Icon(Icons.history, color: _currentIndex == 1 ? ThemeColor.accent : ThemeColor.black),
                   ),
                   const SizedBox(width: 42),
                   IconButton(
-                    onPressed: () => _setIndex(2, itemWidth),
+                    onPressed: () {
+                      _setIndex(2, itemWidth);
+                      _pageController.animateToPage(2, duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+                    },
                     icon: Icon(Icons.notifications, color: _currentIndex == 2 ? ThemeColor.accent : ThemeColor.black),
                   ),
                   IconButton(
-                    onPressed: () => _setIndex(3, itemWidth),
+                    onPressed: () {
+                      _setIndex(3, itemWidth);
+                      _pageController.animateToPage(3, duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+                    },
                     icon: Icon(Icons.person, color: _currentIndex == 3 ? ThemeColor.accent : ThemeColor.black),
                   ),
                 ],
@@ -89,60 +119,58 @@ class _HomeState extends State<Home> {
         body: Container(
             height: MediaQuery.of(context).size.height,
             color: ThemeColor.primary,
-            child: SingleChildScrollView(
-                padding: const EdgeInsets.only(right: 15, left: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    ListTile(
-                      title: const Text(
-                        'WELCOME,',
-                        style: TextStyle(color: ThemeColor.tertiary, fontSize: 12),
-                      ),
-                      subtitle: Text(
-                        AthleteModel.current!.firstName.toUpperCase(),
-                        style: const TextStyle(
-                          color: ThemeColor.white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w700,
-                          letterSpacing: 0.8,
-                        ),
-                      ),
-                      trailing: CircleAvatar(
-                        backgroundColor: ThemeColor.white,
-                        child: Text(
-                          AthleteModel.current!.firstName.characters.first + AthleteModel.current!.lastName.characters.first,
-                          style: const TextStyle(
-                            color: ThemeColor.secondary,
-                            fontWeight: FontWeight.bold,
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (value) => _setIndex(value, itemWidth),
+              children: [
+                SingleChildScrollView(
+                    padding: const EdgeInsets.only(right: 15, left: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ListTile(
+                          title: const Text(
+                            'WELCOME,',
+                            style: TextStyle(color: ThemeColor.tertiary, fontSize: 12),
+                          ),
+                          subtitle: Text(
+                            AthleteModel.current!.firstName.toUpperCase(),
+                            style: const TextStyle(
+                              color: ThemeColor.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 0.8,
+                            ),
+                          ),
+                          trailing: CircleAvatar(
+                            backgroundColor: ThemeColor.white,
+                            child: Text(
+                              AthleteModel.current!.firstName.characters.first + AthleteModel.current!.lastName.characters.first,
+                              style: const TextStyle(
+                                color: ThemeColor.secondary,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    const RecentWorkout(),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
+                        const SizedBox(height: 15),
+                        const RecentWorkout(),
+                        const SizedBox(height: 30),
                         const Text(
-                          'PERSONAL RECORDS',
+                          'RECENT PERSONAL RECORDS',
                           style: TextStyle(
                             color: ThemeColor.white,
                             fontSize: 14,
                             fontWeight: FontWeight.w700,
                           ),
                         ),
-                        IconButton(
-                          onPressed: () => Routes.to(context, RouteList.personal_record, 'right'),
-                          icon: const Icon(Icons.more_horiz, color: ThemeColor.white),
-                        )
+                        const SizedBox(height: 20),
+                        const PersonalRecordsGrid(),
                       ],
-                    ),
-                    const SizedBox(height: 5),
-                    const PersonalRecordsGrid(),
-                  ],
-                ))),
+                    )),
+                const PRHome(),
+              ],
+            )),
       ),
     );
   }
--- lib/widgets/user/personal_records/pr_home.dart
+++ lib/widgets/user/personal_records/pr_home.dart
@@ -15,6 +15,7 @@ class PRHome extends StatefulWidget {
 class _PRHomeState extends State<PRHome> {
   final List<PersonalRecordModel> _pr = PersonalRecordModel.list;
   final List<PRHistoryModel> _prHistory = PRHistoryModel.list;
+  final ScrollController _scrollController = ScrollController();
 
   String _dateFormat(DateTime date) {
     return DateFormat.yMMMMd('en_US').format(date);
@@ -27,107 +28,92 @@ class _PRHomeState extends State<PRHome> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Personal Records'),
-        centerTitle: true,
-        backgroundColor: ThemeColor.primary,
-        foregroundColor: ThemeColor.white,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: const Icon(Icons.arrow_back_ios),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.edit),
-          ),
-        ],
-      ),
-      body: Container(
-          padding: const EdgeInsets.all(20.0),
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [ThemeColor.primary, ThemeColor.secondary],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+  Widget build(BuildContext context) => Container(
+      padding: const EdgeInsets.all(20.0),
+      color: ThemeColor.primary,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'PERSONAL RECORD',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: ThemeColor.white,
             ),
           ),
-          child: Column(
-            children: [
-              SearchBar(
-                hintText: 'Search personal records',
-                leading: const Icon(Icons.search),
-                constraints: const BoxConstraints(
-                  minHeight: 50,
-                  maxHeight: 50,
-                ),
-                textStyle: MaterialStateProperty.all(const TextStyle(
-                  fontSize: 12,
-                  color: ThemeColor.black,
-                )),
-                padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 10)),
-                onChanged: (value) {},
-              ),
-              const SizedBox(height: 20),
-              (_pr.isEmpty)
-                  ? const EmptyContent(title: 'No personal records', subtitle: 'Add your personal records or no search results found')
-                  : Expanded(
-                      child: ListView.builder(
-                          itemCount: _pr.length,
-                          itemBuilder: (context, int index) {
-                            return Card(
-                                elevation: 2,
-                                color: ThemeColor.accent,
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 12,
-                                    horizontal: 16,
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            _pr[index].title,
-                                            style: const TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.w700,
-                                              color: ThemeColor.black,
-                                            ),
-                                          ),
-                                          Text(
-                                            'Created ${_dateFormat(_pr[index].createdAt)}',
-                                            style: const TextStyle(
-                                              fontSize: 11,
-                                              color: ThemeColor.black,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 10),
-                                          Text(
-                                            'Histories: ${_historiesCount(_pr[index].id)}',
-                                            style: TextStyle(
-                                              fontSize: 10,
-                                              color: ThemeColor.black.withOpacity(0.9),
-                                            ),
-                                          )
-                                        ],
+          const SizedBox(height: 20),
+          SearchBar(
+            hintText: 'Search personal records',
+            leading: const Icon(Icons.search),
+            constraints: const BoxConstraints(
+              minHeight: 50,
+              maxHeight: 50,
+            ),
+            textStyle: MaterialStateProperty.all(const TextStyle(
+              fontSize: 12,
+              color: ThemeColor.black,
+            )),
+            padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 10)),
+            onChanged: (value) {},
+          ),
+          const SizedBox(height: 20),
+          (_pr.isEmpty)
+              ? const EmptyContent(title: 'No personal records', subtitle: 'Add your personal records or no search results found')
+              : Expanded(
+                  child: ListView.separated(
+                    itemCount: _pr.length,
+                    itemBuilder: (context, int index) {
+                      return Card(
+                          elevation: 2,
+                          color: ThemeColor.accent,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 16,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      _pr[index].title,
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w700,
+                                        color: ThemeColor.black,
                                       ),
-                                      IconButton(
-                                        onPressed: () {},
-                                        icon: const Icon(Icons.arrow_forward_ios_rounded),
+                                    ),
+                                    Text(
+                                      'Created ${_dateFormat(_pr[index].createdAt)}',
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        color: ThemeColor.black,
                                       ),
-                                    ],
-                                  ),
-                                ));
-                          }),
-                    )
-            ],
-          )),
-    );
-  }
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      'Histories: ${_historiesCount(_pr[index].id)}',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: ThemeColor.black.withOpacity(0.9),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.arrow_forward_ios_rounded),
+                                ),
+                              ],
+                            ),
+                          ));
+                    },
+                    separatorBuilder: (context, int index) => const SizedBox(height: 4),
+                  ),
+                )
+        ],
+      ));
 }
