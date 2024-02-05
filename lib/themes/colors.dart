d3cd1a85801a7dfbbe1eb77f468bffc0f3976d8a
faustojc <faustojcboko@gmail.com>
2024-02-05 22:14:42 +0800

    Change accent color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -4,7 +4,7 @@ class ThemeColor {
   static const Color primary = Color(0xFF27374D);
   static const Color secondary = Color(0xFF526D82);
   static const Color tertiary = Color(0xFF9DB2BF);
-  static const Color accent = Color(0xFFC3D3F3);
+  static const Color accent = Color(0xFFB4DDFD);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF0A1415);
 }
