2630f117180e8d49042bb193da540808f157656e
faustojc <faustojcboko@gmail.com>
2024-02-08 18:45:00 +0800

    Fixed setState issue

--- lib/widgets/components/splash_screen_loading.dart
+++ lib/widgets/components/splash_screen_loading.dart
@@ -21,7 +21,7 @@ class _SplashScreenLoadingState extends State<SplashScreenLoading> {
   void initState() {
     super.initState();
 
-    _checkAuthAndFetch();
+    WidgetsBinding.instance.addPostFrameCallback((_) async => await _checkAuthAndFetch());
   }
 
   Future<void> _checkAuthAndFetch() async {
@@ -30,11 +30,12 @@ class _SplashScreenLoadingState extends State<SplashScreenLoading> {
 
       if (mounted) {
         Navigator.of(context).pushReplacementNamed(RouteList.home.name);
-      }
 
-      return;
+        return;
+      }
     } else if (mounted) {
       Navigator.of(context).pushReplacementNamed(RouteList.login.name);
+      return;
     }
 
     return;
@@ -42,23 +43,21 @@ class _SplashScreenLoadingState extends State<SplashScreenLoading> {
 
   Future<void> _fetchData() async {
     // Use only for virtual device testing
-    // AthleteModel.current = AthleteModel.fromJson(await supabase.from("athletes").select().eq("userId", user.id).single());
-    // SubscriptionModel.current = SubscriptionModel.fromJson(await supabase.from("subscriptions").select().eq("userId", user.id).single());
-    // PersonalRecordModel.list = PersonalRecordModel.fromList(await supabase.from("personal_records").select().eq("userId", user.id));
-    // PRHistoryModel.list = PRHistoryModel.fromList(await supabase.from("personal_records_history").select().eq("userId", user.id));
-    // UserWorkoutModel.list = UserWorkoutModel.fromList(await supabase.from("user_workouts").select().eq("userId", user.id));
+    AthleteModel.current = AthleteModel.fromJson(await supabase.from("athletes").select().eq("userId", user.id).single());
+    SubscriptionModel.current = SubscriptionModel.fromJson(await supabase.from("subscriptions").select().eq("userId", user.id).single());
+    PersonalRecordModel.list = PersonalRecordModel.fromList(await supabase.from("personal_records").select().eq("userId", user.id));
+    PRHistoryModel.list = PRHistoryModel.fromList(await supabase.from("personal_records_history").select().eq("userId", user.id));
+    UserWorkoutModel.list = UserWorkoutModel.fromList(await supabase.from("user_workouts").select().eq("userId", user.id));
 
     // Use for physical device testing
-    AthleteModel.current = await AthleteModel.getByUserId(user.id);
-    SubscriptionModel.current = await SubscriptionModel.getSingleByUserId(user.id);
-
-    PersonalRecordModel.list = await PersonalRecordModel.getAllByUserId(user.id);
-    PRHistoryModel.list = await PRHistoryModel.getAllByUserId(user.id);
-    UserWorkoutModel.list = await UserWorkoutModel.getAllByUserId(user.id);
+    // AthleteModel.current = await AthleteModel.getByUserId(user.id);
+    // SubscriptionModel.current = await SubscriptionModel.getSingleByUserId(user.id);
+    //
+    // PersonalRecordModel.list = await PersonalRecordModel.getAllByUserId(user.id);
+    // PRHistoryModel.list = await PRHistoryModel.getAllByUserId(user.id);
+    // UserWorkoutModel.list = await UserWorkoutModel.getAllByUserId(user.id);
   }
 
   @override
-  Widget build(BuildContext context) {
-    return const Loading();
-  }
+  Widget build(BuildContext context) => const Loading();
 }
