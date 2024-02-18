dfb4fc5c9316c5c5137514fe7fcb83f3a0e10931
faustojc <faustojcboko@gmail.com>
2024-02-18 17:54:05 +0800

    Change to listen for internet connection

--- lib/services/connectivity_service.dart
+++ lib/services/connectivity_service.dart
@@ -4,11 +4,11 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
-enum InternetStatus { online, offline }
-
 class ConnectivityService extends ChangeNotifier {
   late StreamSubscription<ConnectivityResult> _result;
-  InternetStatus status = InternetStatus.offline;
+  late StreamSubscription<InternetStatus> _internetListener;
+
+  InternetStatus status = InternetStatus.connected;
 
   ConnectivityService() {
     _result = Connectivity().onConnectivityChanged.listen(_updateConnectionStatus);
@@ -18,11 +18,13 @@ class ConnectivityService extends ChangeNotifier {
     switch (result) {
       case ConnectivityResult.wifi:
       case ConnectivityResult.mobile:
-        status = await InternetConnection().hasInternetAccess ? InternetStatus.online : InternetStatus.offline;
+        _internetListener = InternetConnection().onStatusChange.listen(
+              (event) => status = (event == InternetStatus.connected) ? InternetStatus.connected : InternetStatus.disconnected,
+            );
         break;
       case ConnectivityResult.none:
       default:
-        status = InternetStatus.offline;
+        status = InternetStatus.disconnected;
         break;
     }
     notifyListeners();
@@ -31,6 +33,7 @@ class ConnectivityService extends ChangeNotifier {
   @override
   void dispose() {
     _result.cancel();
+    _internetListener.cancel();
     super.dispose();
   }
 }
