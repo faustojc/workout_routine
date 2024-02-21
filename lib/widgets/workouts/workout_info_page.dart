3d9a89681cc4968d22882bfb3a4c2cf00bf4543b
faustojc <faustojcboko@gmail.com>
2024-02-21 22:15:59 +0800

    Add workout info and workout type pages

---
+++ lib/widgets/workouts/workout_info_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WorkoutInfoPage extends StatelessWidget {
+  const WorkoutInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/workouts/workout_type_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorkoutTypePage extends StatefulWidget {
+  const WorkoutTypePage({super.key});
+
+  @override
+  State<WorkoutTypePage> createState() => _WorkoutTypePageState();
+}
+
+class _WorkoutTypePageState extends State<WorkoutTypePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
