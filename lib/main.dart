cbe940c5dce34bb7ef266f47030bb88d87cafad5
faustojc <faustojcboko@gmail.com>
2024-02-27 12:51:01 +0800

    Using uni_link to listen for deep links

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workout_routine/backend/powersync.dart';
 import 'package:workout_routine/routes.dart';
+import 'package:workout_routine/services/auth_state_provider.dart';
 import 'package:workout_routine/services/connectivity_service.dart';
 import 'package:workout_routine/themes/colors.dart';
 import 'package:workout_routine/widgets/components/splash_screen_loading.dart';
@@ -12,18 +13,21 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await openDatabase();
 
-  runApp(const MainApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => ConnectivityService()),
+      ChangeNotifierProvider(create: (_) => AuthStateProvider()),
+    ],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  // This widget is the root of your application.
+  // Root of the application
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<ConnectivityService>(
-      create: (context) => ConnectivityService(),
-      child: MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
         title: 'Strength and Conditioning',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: ThemeColor.primary),
@@ -39,7 +43,5 @@ class MainApp extends StatelessWidget {
         onGenerateRoute: Routes.generateRoute,
         debugShowCheckedModeBanner: false,
         home: const SplashScreenLoading(),
-      ),
-    );
-  }
+      );
 }
