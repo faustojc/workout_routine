dedec7a04d6ff0b7c533dacff318cdac7cf6a67f
faustojc <faustojcboko@gmail.com>
2024-03-08 21:23:55 +0800

    Changed theme color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 
 class ThemeColor {
-  static const Color primary = Color(0xFF27374D);
+  static const Color primary = Color(0xFFE0FE10);
   static const Color secondary = Color(0xFF526D82);
   static const Color tertiary = Color(0xFF9DB2BF);
   static const Color accent = Color(0xFFB4DDFD);
   static const Color white = Color(0xFFFFFFFF);
-  static const Color black = Color(0xFF0A1415);
+  static const Color black = Color(0xFF0B0B0B);
+  static const Color grey = Color(0xFF212326);
 }
